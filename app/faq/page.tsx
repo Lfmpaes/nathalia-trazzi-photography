ce04e9d082adaae46026fc6858a6a12df842e1cd
Luiz Fernando Paes <luiz@lfmpaes.com.br>
2023-06-25 17:19:10 -0300

    Added faq and portfolio default functions

---
+++ app/faq/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function Faq() {
+    return (
+        <main className="main">
+            <h1>Faq Page</h1>
+            <Link href="/">Home</Link>
+        </main>
+    );
+}
---
+++ app/portfolio/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function Portfolio() {
+    return (
+        <main className="main">
+            <h1>Portfolio Page</h1>
+            <Link href="/">Home</Link>
+        </main>
+    );
+}
