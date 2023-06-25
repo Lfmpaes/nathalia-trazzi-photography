ee059bfc6f143000f70f1d0d8f00ba384446b751
Luiz Fernando Paes <luiz@lfmpaes.com.br>
2023-06-25 17:55:29 -0300

    Refatorado pra fazer funcionar

--- app/components/homePage.tsx
+++ app/components/homePage.tsx
@@ -1,49 +1,43 @@
-import React from "react"
-import Link from "next/link"
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 
 export const HeaderComponent: React.FC = () => {
     return (
         <header>
             <nav className="nav-links">
-                <a href="/sobre"> Sobre mim </a> 
-                <a href="/still"> Still </a> 
-                <a href="/studio"> Studio </a> 
-                <a href="/fineArt"> Fine Art </a> 
+                <a href="/sobre"> Sobre mim </a>
+                <a href="/still"> Still </a>
+                <a href="/studio"> Studio </a>
+                <a href="/fineArt"> Fine Art </a>
             </nav>
         </header>
-    )
-}
+    );
+};
 
 export const SlideComponent: React.FC = () => {
     return (
         <section className="slide-container">
             <div className="slide">
-                <image> </image>
-
+                <Image src="/favicon.ico" alt="Hey" width={32} height={32} />
             </div>
             <div className="slide2">
-                <image> </image>
-
+                <Image src="/favicon.ico" alt="Hey" width={32} height={32} />
             </div>
             <div className="slide3">
-                <image> </image>
-
+                <Image src="/favicon.ico" alt="Hey" width={32} height={32} />
             </div>
             <div className="slide4">
-                <image> </image>
-
+                <Image src="/favicon.ico" alt="Hey" width={32} height={32} />
             </div>
         </section>
-
-    )
-    
-}
+    );
+};
 
 export const FooterComponent: React.FC = () => {
     return (
         <footer className="footer-container">
             <p> Nathalia Trazzi Photography</p>
         </footer>
-    )
-}
-
+    );
+};
