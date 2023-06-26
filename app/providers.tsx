1b679d271fb6c82fc6f3b6cd5d80a1099cb7f198
Luiz Fernando Paes <luiz@lfmpaes.com.br>
2023-06-26 10:01:54 -0300

    Another try to fix SSRProvider bug

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,18 +1,21 @@
 'use client';
 
-import { CssBaseline, NextUIProvider } from '@nextui-org/react';
+import { CssBaseline, NextUIProvider, useSSR } from '@nextui-org/react';
 import Header from './components/Header';
 import React from 'react';
 import './globals.css';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
+    const { isBrowser } = useSSR();
     return (
-        <NextUIProvider>
-            <div className="app">
-                {CssBaseline.flush()}
-                <Header />
-                <main>{children}</main>
-            </div>
-        </NextUIProvider>
+        isBrowser && (
+            <NextUIProvider>
+                <div className="app">
+                    {CssBaseline.flush()}
+                    <Header />
+                    <main>{children}</main>
+                </div>
+            </NextUIProvider>
+        )
     );
 }
